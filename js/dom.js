0d5b140c811dd339e4ad463969dd4590a5d96f45
SQLAdmin <abuyeahia24@gmail.com>
2023-02-20 00:44:53 +0600

    added new line and change color with js btn click

--- js/dom.js
+++ js/dom.js
@@ -10,4 +10,14 @@ document.getElementById('apply-bg').addEventListener('click', function(){
 document.getElementById('center-third').addEventListener('click', function(){
     const third = document.getElementById('third-friend')
     third.style.textAlign = 'center';
+});
+document.getElementById('add-friend').addEventListener('click', function(){
+    const friendContainer = document.getElementById('friends');
+    const friend = document.createElement('div');
+    friend.classList.add('friend');
+    friend.innerHTML = `
+    <h3 class = "friend-name">New friend</h3>
+    <p>something new added</p>
+    `
+    friendContainer.appendChild(friend);
 })
