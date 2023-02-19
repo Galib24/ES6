146d6cb12efa9d07fe50621d0e75e306d2b4653e
SQLAdmin <abuyeahia24@gmail.com>
2023-02-20 00:35:16 +0600

    center add text by js

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,13 @@
 document.getElementById('apply-bg').addEventListener('click', function(){
-    console.log('click')
+    // console.log('click')
+    const friends = document.getElementsByClassName('friend');
+    // console.log(friends)
+    for(const friend of friends){
+        // console.log(friend)
+        friend.style.backgroundColor = 'lightblue'
+    }
+});
+document.getElementById('center-third').addEventListener('click', function(){
+    const third = document.getElementById('third-friend')
+    third.style.textAlign = 'center';
 })
